7bf2c34e652907e1d4457e5963f897dd0575e61d
Asinsa <asinsa.g@gmail.com>
2022-11-01 17:33:06 +0000

    Functions for User to query Profile &the other way

--- database/migrations/2022_11_01_021258_create_profiles_table.php
+++ database/migrations/2022_11_01_021258_create_profiles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->string('profile_pic');
+            $table->string('profile_pic')->nullable();
             $table->timestamps();
 
             $table->bigInteger('user_id')->references('id')->on('users')->onDe1ete('cascade')->onUpdate('cascade');
