6f50823827cb73f3542b6c271612953dfb09c853
Asinsa <asinsa.g@gmail.com>
2022-11-02 09:11:12 +0000

    Added seeder to User & hardcoded an example

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Hardcoded User
+        $a = new User();
+        $a->name = "Shadow Shadow";
+        $a->email = "shadow@gmail.com";
+        $a->password = Hash::make('password');
+        $a->save();
+    }
+}
