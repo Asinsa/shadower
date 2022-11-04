c126a13d45129f72884be5bbbfc8b97e91c9535f
Asinsa <asinsa.g@gmail.com>
2022-11-04 20:03:22 +0000

    Forgot to commit calling the ProfileTableSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserTableSeeder::class);
         $this->call(ProfileTableSeeder::class);
+        $this->call(PostTableSeeder::class);
+
     }
 }
