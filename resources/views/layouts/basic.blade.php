f0c46b42215779486424864a42278596ee139cca
Asinsa <asinsa.g@gmail.com>
2022-12-19 20:01:03 +0000

    Made basic layout file

---
+++ resources/views/layouts/basic.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Shadower - @yeild('title')</title>
+</head>
+
+<body>
+    <h1>Shadower - @yeild('title')</h1>
+    
+    <div>
+        @yield('content')
+    </div>
+
+ </html>
