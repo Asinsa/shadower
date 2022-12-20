6d3b0a5e4d4faf8de3e1fe7cfb8e294ebd27eeac
Asinsa <asinsa.g@gmail.com>
2022-12-20 19:17:34 +0000

    Made show view for post so it doesn't show up as just a json string

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Show Post View')
 
 @section('content')
-    {{$post}}
+    <ul>
+        <li>Title: {{$post->title}}</li>
+        <li>Image: {{$post->image}}</li>
+        <li><img src={{$post->image}} alt="Image" width="300" height="300"></li>
+        <li>Body: {{$post->body}}</li>
+    </ul>
 
 @endsection
