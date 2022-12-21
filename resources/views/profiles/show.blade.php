3af31664cf725ee4c692cf96d69af6f2a05cea48
Asinsa <asinsa.g@gmail.com>
2022-12-21 22:48:15 +0000

    Posts and comments viewable on profile view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,4 +9,22 @@
         <li><img src={{$profile->profile_pic}} alt="Image" width="300" height="300"></li>
     </ul>
 
+    <h2>Posts by {{$profile->username}}</h2>
+    <ul>
+        @foreach ($profile->posts as $post)
+            <li><a href='{{ route('posts.show', ['id' => $post->id]) }}'>{{ $post->title }}</a></li>
+        @endforeach
+    </ul>
+
+    <h2>Comments by {{$profile->username}}</h2>
+    <ul>
+        @foreach ($profile->interactions as $interaction)
+            @if ( $interaction->interaction_type == "comment")
+                <li>"{{ $interaction->comment }}" 
+                on the post "<a href='{{ route('posts.show', ['id' => $interaction->post->id]) }}'>{{ $interaction->post->title }}</a>" 
+                at {{$interaction->created_at}}.</li>
+            @endif
+        @endforeach
+    </ul>
+
 @endsection
