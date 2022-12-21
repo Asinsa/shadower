88045a282b6a1003d7c0979627e41cf600a0d845
Asinsa <asinsa.g@gmail.com>
2022-12-21 22:56:18 +0000

    Comments of post viewable on post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,4 +11,15 @@
         <li>Poster: <a href='{{ route('profiles.show', ['id' => $post->profile->id]) }}'>{{ $post->profile->username }}</a></li>
     </ul>
 
+    <h2>Comments</h2>
+    <ul>
+        @foreach ($post->interactions as $interaction)
+            @if ( $interaction->interaction_type == "comment")
+                <li><a href='{{ route('profiles.show', ['id' => $interaction->profile->id]) }}'>{{ $interaction->profile->username }}</a>
+                 commented "{{ $interaction->comment }}"
+                 at {{$interaction->created_at}}.</li>
+            @endif
+        @endforeach
+    </ul>
+
 @endsection
