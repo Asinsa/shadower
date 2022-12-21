52c25bd33b7cb7dda88f0c157bc6c3d3476b6f94
Asinsa <asinsa.g@gmail.com>
2022-12-21 21:51:38 +0000

    Made users clickable by hardcoding

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
     <p>All users:</p>
     <ul>
         @foreach ($users as $user)
-            <li>{{ $user->name }}</li>
+            <li><a href='/users/{{$user->id}}'>{{ $user->name }}</a></li>
         @endforeach
     </ul>
 
