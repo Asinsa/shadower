b091123f32c6076003057d48edd8d74b73de4939
Asinsa <asinsa.g@gmail.com>
2022-12-31 06:01:51 +0000

    Set default value of new post fields to be old input values

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,9 +5,12 @@
 @section('content')
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
-        <p>Title: <input type="text" name="title"></p>
-        <p>Image Link: <input type="text" name="image"></p>
-        <p>Body: <input type="text" name="body"></p>
+        <p>Title: <input type="text" name="title"
+            value="{{ old('title') }}"></p>
+        <p>Image Link: <input type="text" name="image"
+            value="{{ old('image') }}"></p>
+        <p>Body: <input type="text" name="body"
+            value="{{ old('body') }}"></p>
         <input type="submit" value="Submit">
         <a href="{{ route('posts.index') }}">Cancel</a>
     </form>
